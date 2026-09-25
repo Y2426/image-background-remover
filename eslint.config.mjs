f0831532575008e5ad196fdb74ee26314db54da3
Y2426 <177808231+Y2426@users.noreply.github.com>
2026-09-25 11:49:03 +0800

    Ignore static export in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["next-env.d.ts", ".next/**"],
+    ignores: ["next-env.d.ts", ".next/**", "out/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
